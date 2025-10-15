cd159b02073dcb761aa464d28a8460dc2313f9db
NAMKYU\rlask <rlaskarb20@gmail.com>
2025-10-15 13:15:37 +0900

    10/15

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ const navMenuItems = [
 
 
 const headerShortcuts = [
-    { href: "#2section", src: "./images/1.png", alt: "프로필 바로가기" },
-    { href: "#3section", src: "./images/2.png", alt: "pc 프로젝트 바로가기" },
-    { href: "#5section", src: "./images/5.png", alt: "반응형 프로젝트 바로가기" },
+    { href: "#2section", src: "./images/1.avif", alt: "프로필 바로가기" },
+    { href: "#3section", src: "./images/2.avif", alt: "pc 프로젝트 바로가기" },
+    { href: "#5section", src: "./images/5.avif", alt: "반응형 프로젝트 바로가기" },
 ];
 
 
@@ -31,41 +31,41 @@ const chatListData = [
     {
         id: "friend",
         title: " 친구 4",
-        lastMessage: "와... 대박 남규야 ...",
+        lastMessage: "면접 잘봄?",
         time: "오후 2:40",
         unread: 0,
-        avatar: "./images/nam.jpg",
+        avatar: "./images/6.avif",
         mediaType: 'image',
         isClickable: false
     },
     {
-        id: "kakaotalk-deal",
-        title: " 카카오 톡딜",
-        lastMessage: "(광고)[톡딜 마감 임박 알림] 김남규 포트폴리오...",
+        id: "7-eleven",
+        title: "7-ELEVEn",
+        lastMessage: "Supabase를 사용하셨던데...",
         time: "오전 12:30",
         unread: 0,
-        avatar: "./images/nam.jpg",
+        avatar: "./images/2.avif",
         mediaType: 'image',
         isClickable: false
     },
     {
         id: "kakao-pay",
-        title: "NK 페이",
-        lastMessage: "김남규 포트폴리오가 완료되었어요",
+        title: "ZOEY",
+        lastMessage: "이미지와 영상을 144개 사용했는데 Lighthouse 점수가 90..",
         time: "오전 08:21",
         unread: 0,
-        avatar: "./images/nam.jpg",
+        avatar: "./images/zoey.avif",
         mediaType: 'image',
         isClickable: false
     },
 
     {
-        id: "nobfshim",
-        title: "KNK 브랜드 스토어",
-        lastMessage: "포트폴리오 조회 버튼을 누르면 남규의현황을...",
+        id: "gunness",
+        title: "GUNNESS",
+        lastMessage: "React로 ERP 프로그램을 만드셨다면서요!",
         time: "오전 08:00",
         unread: 0,
-        avatar: "./images/nam.jpg",
+        avatar: "./images/gunness.avif",
         mediaType: 'image',
         isClickable: false
     },
@@ -76,9 +76,16 @@ const chatListData = [
 
 const kakaotalkData = {
     "portfolio-intro": [ //chatListData 의 id 연결
-        { speaker: '망붕이', avatar: "./images/nam.jpg", message: '남규 포트폴리오를 클릭해주셔서 감사합니다' },
-        { speaker: '김남규', message: '제 포트폴리오를 소개하겠습니다' },
-        { speaker: '김남규', avatar: "./images/nam.jpg", message: '<a href="#2section">프로필 바로가기</a>' }
+        { speaker: '김남규',
+            avatar: "./images/1.avif",
+            mediaType: 'image',
+            message: '김남규 포트폴리오를 클릭해주셔서 감사합니다' },
+
+        { speaker: '김남규1',
+             message: '제 소개를 시작을 하겠습니다.' },
+
+        { speaker: '김남규1',
+            message: '<a href="#2section">프로필 바로가기</a>' }
     ],
 };
 
@@ -113,6 +120,10 @@ function createChatListHeader() {
     const header = document.querySelector('.chat-list-panel .kakakotalk-header');
     if (!header) return;
 
+    const totalUnread = chatListData.reduce(function(sum,item){
+      return  sum + item.unread
+    },0);
+
     const iconsHTML = headerShortcuts.map(function (shortcut) {
         return `      
         <a href="${shortcut.href}" >
@@ -125,8 +136,8 @@ function createChatListHeader() {
         <div class="main-title"> 
            <span class="header-title">채팅</span>
            <ul>
-           <li><a href="#">전체</a></li>
-           <li><a href="#">안읽음</a></li>
+           <li><a href="#" id="filter-all">전체</a></li>
+           <li><a href="#" id="filter-unread" >안읽음  ${totalUnread > 0 ? `<span class="unread-count">${totalUnread}</span>` : ''}</a></li>
            <li><a href="#"><i class="fa-solid fa-bars"></i></a></li>
            </ul>
         </div>
@@ -136,12 +147,12 @@ function createChatListHeader() {
 }
 
 
-function createChatList() {
+function createChatList(dataToRender) {
     const chatListContainer = document.querySelector('.chat-list');
 
     if (!chatListContainer) return;
 
-    chatListContainer.innerHTML = chatListData.map(function (item) {
+    chatListContainer.innerHTML = dataToRender.map(function (item) {
         let avatarTagHTML;
         if (item.mediaType === "video") {
             avatarTagHTML = `
@@ -191,6 +202,8 @@ function addChatClickEvent() {
 
         chatListData[0].unread = 0;
 
+        updataHeaderCount();
+
         const unreadBadge = firstChatItem.querySelector('.unread-count');
 
         if (unreadBadge) {
@@ -220,31 +233,49 @@ function addChatClickEvent() {
 function createKakaotalkUI(chatroomId) {
     const messageContainer = document.querySelector('.kakaotalk-messages');
     const conversationHeader = document.querySelector('.conversation-panel .kakakotalk-header');
-
     if (!messageContainer || !conversationHeader) return;
 
     const messages = kakaotalkData[chatroomId];
-
     const chatroomInfo = chatListData.find(function (item) {
-
         return item.id === chatroomId;
-
     });
-
     if (!chatroomInfo || !messages) return;
 
-    conversationHeader.innerHTML = `
-        <img src="${chatroomInfo.avatar}" alt="${chatroomInfo.title}" class="header-avatar">
-        <span id="conversation-title">${chatroomInfo.title}</span> `;
+    let headerAvatarHTML;
 
+    if(chatroomInfo.mediaType==='video'){
+        headerAvatarHTML = `
+            <video autoplay loop muted class="header-avatar">
+                <source src="${chatroomInfo.avatar}" type="video/webm">
+            </video>`;
+    }else{
+        headerAvatarHTML = `
+        <img src="${chatroomInfo.avatar}" alt="${chatroomInfo.title}" class="header-avatar">`;
+    }
+
+    conversationHeader.innerHTML =`
+        ${headerAvatarHTML} 
+        <span id="conversation-title">${chatroomInfo.title}</span>
+    `
+    
     messageContainer.innerHTML = messages.map(function (chat) {
-
-        const messageType = (chat.speaker === '김남규') ? 'sent' : 'received';
+        const messageType = (chat.speaker === '김남규1') ? 'sent' : 'received';
 
         if (messageType === "received") {
+
+            let messageAavatarHTML;
+            if( chat.mediaType === 'video'){
+                messageAavatarHTML = `
+                    <video autoplay loop muted class="avatar">
+                        <source src="${chat.avatar}" type="video/webm">
+                    </video>`;
+            }else{
+                messageAavatarHTML = `<img src="${chat.avatar}" alt="${chat.title}" class="header-avatar">`;
+            }
+
             return `
                 <div class="kakaotalk-message-row received">
-                    <img src="${chat.avatar}" alt="${chat.speaker}" class="avatar">
+                    ${messageAavatarHTML}
                     <div class="message-content">
                         <div class="sender-name">${chat.speaker}</div>
                         <div class="kakaotalk-bubble">${chat.message}</div>
@@ -265,14 +296,48 @@ function createKakaotalkUI(chatroomId) {
 }
 
 
+function addFilterEvents(){
+    const filterAllBtn = document.getElementById('filter-all');
+    const filterUnreadBtn = document.getElementById('filter-unread');
+
+    if(!filterAllBtn || !filterUnreadBtn) return;
+
+    filterAllBtn.addEventListener('click' ,function(event){
+        event.preventDefault();
+        createChatList(chatListData);
+    });
+
+    filterUnreadBtn.addEventListener('click' ,function(event){
+        event.preventDefault();
+        const unreadList = chatListData.filter(function(item){
+           return item.unread > 0;
+        });
+        createChatList(unreadList); // 안앍은 목록만 전달
+    });
+}
+
+
+function updataHeaderCount(){
+    const filterUnreadBtn = document.getElementById('filter-unread');
+    if(!filterUnreadBtn) return;
+
+    const totalUnread = chatListData.reduce(function(sum , item){
+        return sum + item.unread;
+    },0);
+
+    filterUnreadBtn.innerHTML = `안읽음 ${totalUnread > 0 ? `<span class="unread-count"></span>` : ''}`;
+}
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
 
     createNavigation();
 
-    createChatList();
+    createChatListHeader(); // 1. 헤더를 먼저 만드어서 버튼들을 생성
 
-    createChatListHeader();
+    createChatList(chatListData); // 2. 처음에는 전체 목록을 보여줌
 
-})
+    addFilterEvents(); // 3. 생성된 버튼들에 클릭기능을 추가
+
+});
